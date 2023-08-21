ce423c9b6631e48d637106f3b3aacaa635341894
HARIS RAHMAN NASUTION <haris.nasution@binus.ac.id>
2023-08-21 11:38:40 +0700

    update dashboard

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->enum("role", ["admin", "staff"]);
+            $table->enum("role", ["admin", "staff"])->default("staff");
             $table->boolean('is_admin')->default(false);
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
